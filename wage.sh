77909e2f1abcae87ee3e5fe6a70c1a6c0241d876
Akshata72 <sableakshata880@gmail.com>
2022-02-21 18:30:37 +0530

    Added:UC2 logic

--- wage.sh
+++ wage.sh
@@ -1,4 +1,5 @@
 
+
 echo "Welcome to Employee Wage Computation Program "
 
 random_num=$(( RANDOM%2 ))
@@ -6,6 +7,22 @@ random_num=$(( RANDOM%2 ))
 if (($random_num==1))
 then
   echo "Employee is Present"
+
+fi
+isFullTime=1
+empRateperHr=20
+
+random_check=$(( RANDOM%3 ))
+if (( $random_check==$isFullTime ))
+
+then
+     empHrs=8
+
+salary=$(($empRateperHr*$empHrs))
+
+echo "$salary"
+
+
 else
   echo "Employee is Absent"
 fi
