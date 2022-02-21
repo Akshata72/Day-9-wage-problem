f1a351c8ac538d789415fd90b46efbac60d01e62
Akshata72 <sableakshata880@gmail.com>
2022-02-21 21:04:37 +0530

    Added:UC7 logic

--- wage.sh
+++ wage.sh
@@ -22,11 +22,12 @@ for ((i=1 ; i<=20 ; i++))
 do
     res=$(($empRateperHr*$empHrs))
 
-    echo "days:"$i=$(($res*$i))
+    echo "Salary per day:"$i=$(($res*$i)) "Rs"
 
+
+
+echo " working hours=" $(($i*$empHrs)) "Hrs"
 done
-
-
 salary_per_month=$(($empRateperHr*$empHrs*$workingday_per_month))
 
 
