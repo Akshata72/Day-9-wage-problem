255c8063773fba07c5b860dfbcfce1ee9819572d
Akshata72 <sableakshata880@gmail.com>
2022-02-21 18:58:52 +0530

    Added:UC4 logic

--- wage.sh
+++ wage.sh
@@ -1,30 +1,21 @@
 
-
-echo "Welcome to Employee Wage Computation Program "
-
-random_num=$(( RANDOM%2 ))
-
-if (($random_num==1))
-then
-  echo "Employee is Present"
-
-fi
 isFullTime=1
 isPartTime=2
 empRateperHr=20
+emp_check=$(( RANDOM%3 ))
 
-random_check=$(( RANDOM%3 ))
-if (( $random_check==$isFullTime ))
-
-then
-     empHrs=8
-elif (( $random_check==$isPartTime ))
-
-then
-     empHrs=4
-else
-     empHrs=0
-fi
+case $emp_check in
+     
+     $isFullTime)
+            empHrs=8
+               ;;
+     $isPartTime)
+            empHrs=4
+               ;;
+     *)
+            empHrs=0
+               ;;
+esac
 
 salary=$(($empRateperHr*$empHrs))
 
